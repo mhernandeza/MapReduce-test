97eeb686ffd40bbf3ad367d4c9c9dd70e9b9ec16
Margarita Hernandez <margara_ha@hotmail.com>
2017-01-06 18:15:01 -0600

    Fixed build

--- src/main/java/com/base22/VoteCount.java
+++ src/main/java/com/base22/VoteCount.java
@@ -7,6 +7,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
@@ -39,10 +40,10 @@ public class VoteCount extends Configured implements Tool {
 		job.setOutputFormatClass( TextOutputFormat.class );
 
 		FileInputFormat.setInputPaths( job, new Path( args[0] ) );
-
+		FileOutputFormat.setOutputPath( job, new Path( args[1] ) );
 		job.setJarByClass( VoteCount.class );
 
 		job.submit();
-		return 0;
+		return job.waitForCompletion( true ) ? 0 : 1;
 	}
 }
